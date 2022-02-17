60d93cb3e80bd71ccaa9cb599f547e2a3284b89e
kevvintha <63698561+kevvintha@users.noreply.github.com>
2022-02-17 07:27:19 -0800

    changing student to custoemr

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,7 +10,7 @@ class Card extends Component {
         return (
             <div className="card bg-secondary text-white rounded col-lg-2 col-12 col-md-12 d-flex align-content-center" id={'card' + this.props.info.id}>
                 <Header firstName={this.props.info.firstName} lastName={this.props.info.lastName} />
-                <Body url={this.props.info.imagePath} email={this.props.info.email} dept={this.props.info.department} />
+                <Body email={this.props.info.username} />
                 <Footer cardID={'card' + this.props.info.id} student = {this.props.info} studentID={this.props.info.id} handleDelete={this.props.handleDelete} handleUpdate={this.props.handleUpdate} />
             </div>
 
@@ -31,8 +31,7 @@ function Body(props) {
         <div className="card-body">
             <img src={props.url} alt={props.imgAlt} />
             <div className="container">
-                <p><b>Email:</b> {props.email}</p>
-                <p><b>Department:</b> {props.dept}</p>
+                <p><b>Username:</b> {props.email}</p>
             </div>
         </div>
     );
@@ -42,8 +41,8 @@ function Body(props) {
 function Footer(props) {
     return (
         <div className="card-footer btn-group">
-            <button className="btn btn-danger" onClick={props.handleDelete.bind(this, props.studentID)}>DELETE</button>
-            <FormModal type='update' student={props.student} handleUpdate={props.handleUpdate}/>
+            <button className="btn btn-danger" onClick={props.handleDelete.bind(this, props.customerID)}>DELETE</button>
+            <FormModal type='update' customer={props.customer} handleUpdate={props.handleUpdate}/>
         </div>
     );
 }
